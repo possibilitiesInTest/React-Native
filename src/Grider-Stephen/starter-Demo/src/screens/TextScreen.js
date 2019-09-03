47be05c415bd8d533764e76d8522e18d4ecd43f4
Gourav B <pit.wire@pm.me>
2019-09-03 12:58:05 -0400

    text input implementation w. field validation

---
+++ src/Grider-Stephen/starter-Demo/src/screens/TextScreen.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+const TextScreen = () => {
+  const [password, setPassword] = useState("");
+
+  return (
+    <View>
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={newValue => setPassword(newValue)}
+      />
+      {/* <Text>My Name is {name}</Text> */}
+      {password.length < 4 ? <Text>Must be 4 Chars</Text> : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
+
+export default TextScreen;
