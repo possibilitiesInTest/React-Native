78190a4c0ed345f1747d273a157866f6c247e52c
Gourav B <pit.wire@pm.me>
2019-09-04 19:43:10 -0400

    render images on ResultsShowScreen:
    -config withNavitaiton,
    -pass biz.id to ResultsPage
    -async fetch biz route
    -destructure result, setResult from useState passing in
     initial val of null
    -setResult to response of async fetch
    -implement useEffect hook to run search once, w. [] as 2nd arg
     call getResult passing in id
    -render results w. FlatList if available

--- src/Grider-Stephen/food-app/src/components/ResultsList.js
+++ src/Grider-Stephen/food-app/src/components/ResultsList.js
@@ -1,19 +1,37 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
+import { withNavigation } from "react-navigation";
 import ResultsDetail from "./ResultsDetail";
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
-      {/* <Text> Results: {results.length} </Text> */}
       <FlatList
         horizontal
         showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={result => result.id}
         renderItem={({ item }) => {
-          return <ResultsDetail result={item} />;
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("ResultsShow", { id: item.id })
+              }
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
@@ -32,4 +50,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
