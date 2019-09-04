fbb56b41ca481de962fa4024f67d41928ea5a96a
Gourav B <pit.wire@pm.me>
2019-09-04 18:46:17 -0400

    render images, style page elms

--- src/Grider-Stephen/food-app/src/components/ResultsList.js
+++ src/Grider-Stephen/food-app/src/components/ResultsList.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
-      <Text> Results: {results.length} </Text>
+      {/* <Text> Results: {results.length} </Text> */}
       <FlatList
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={result => result.id}
         renderItem={({ item }) => {
-          return <Text>{item.name}</Text>;
+          return <ResultsDetail result={item} />;
         }}
       />
     </View>
@@ -21,7 +23,12 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 10
   }
 });
 
