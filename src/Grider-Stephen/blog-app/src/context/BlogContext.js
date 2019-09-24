96925550335c04933d6b0fa8e3519957801d8100
Gourav B <pit.wire@pm.me>
2019-09-23 23:39:49 -0400

    deleting|editing post to server:
    BlogContext
         -rm: dispatch from addBlogPost
         -rm: addPost case from reducer
         -rf: delete w. async call to server before dispatch
         -rf: edit w. async call to server, pass in {title, content}

--- src/Grider-Stephen/blog-app/src/context/BlogContext.js
+++ src/Grider-Stephen/blog-app/src/context/BlogContext.js
@@ -14,15 +14,15 @@ const blogReducer = (state, action) => {
     case "delete_blogPost":
       return state.filter(blogPost => blogPost.id !== action.payload);
 
-    case "add_blogPost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
+    // case "add_blogPost":
+    //   return [
+    //     ...state,
+    //     {
+    //       id: Math.floor(Math.random() * 99999),
+    //       title: action.payload.title,
+    //       content: action.payload.content
+    //     }
+    //   ];
 
     default:
       return state;
@@ -53,13 +53,16 @@ const addBlogPost = dispatch => {
 };
 
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogPost", payload: id });
   };
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+
     dispatch({
       type: "edit_blogpost",
       payload: { id, title, content }
