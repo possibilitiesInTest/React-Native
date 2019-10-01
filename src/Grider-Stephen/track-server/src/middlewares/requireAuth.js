06851b395bc53f464bf5f2af66d08bbb65234817
Gourav B <pit.wire@pm.me>
2019-10-01 12:50:44 -0400

    salt and hash pw checks w. bcrypt:
    User
         -import bcrypt
         -create pre save hook on userSchema
          pass in save and func(next)
         -reference user from this
         -check if user modifies pw, return next if not
         -else genSalt pass in 10, err, salt
              -call next on err
              -call hash: user.pw, salt, and (err, hash)
              -call next on err
              -else set user.pw to hash
              -call next
    
         -call comparePw on userSchema from methods
         -set to func passing in candidatePw
         -return new Promise
              -call compare: pass candidatePw, user.pw
               and callback w. err and isMatch
              -if err, return reject(err)
              -if not match return reject(false)
              -else resolve(true)

--- src/Grider-Stephen/track-server/src/middlewares/requireAuth.js
+++ src/Grider-Stephen/track-server/src/middlewares/requireAuth.js
@@ -6,16 +6,19 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   // authorization == 'Bearer awidnfo8awhfL23TR'
 
+  //returns err if auth not found
   if (!authorization) {
     return res.status(401).send({ error: "You must be logged in." });
   }
 
+  // extracts token from auth
   const token = authorization.replace("Bearer ", "");
+  // verifies token
   jwt.verify(token, "MY_SECRET_KEY", async (err, payload) => {
     if (err) {
       return res.status(401).send({ error: "You must be logged in." });
     }
-
+    // extracts userId from payload
     const { userId } = payload;
 
     // tells mongoose to look up user from mongo
