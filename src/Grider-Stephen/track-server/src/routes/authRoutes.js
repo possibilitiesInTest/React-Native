07e88a2013a7c0d91b4a248e2e0aa4a7a6dfff18
Gourav B <pit.wire@pm.me>
2019-10-02 13:08:44 -0400

    create tracks model for db:
    models/tracks
         -require mongoose
         -set trackSchema to new mongoose.Schema
         -define userId
              -set type to ObjectId defined in mongoose schema
              -set ref to User (as defnd in User model)
              -def name of type string, set default empt
              -def locations as [pointSchema]
         -set pointSchema as new mongoose.Schema
              -def: timestamp,
                    Coords:
                           lat, long, alt, acc, hding, spd
         -pass mongoose.model for track as trackSchema

--- src/Grider-Stephen/track-server/src/routes/authRoutes.js
+++ src/Grider-Stephen/track-server/src/routes/authRoutes.js
@@ -20,26 +20,25 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-router.post('/signin', (req, res) => {
-   const { email, password } = req.body;
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
 
-   if(!email || !password) {
-     return res.status(422).send({ error: 'Must provide email and password' });
-   }
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
 
-   const user = await User.findOne({ email });
-   if ( !user) {
-    return res.status(422).send({ error: 'Email not found' });
-   }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ error: "Email not found" });
+  }
 
-   try {
-   await user.comparePassword(password);
-    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY")
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
     res.send({ token });
-
-   } catch (err) {
-     return res.status(422).send({ error: 'Invalid password or email' })
-   }
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
 });
 
 module.exports = router;
